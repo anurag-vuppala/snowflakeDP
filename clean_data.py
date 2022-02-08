0d02b7761d3642ea1d0e29c018e4db96704b2307
anurag-vuppala <75685045+anurag-vuppala@users.noreply.github.com>
2022-02-08 20:52:47 +0000

    added u_id column

--- clean_data.py
+++ clean_data.py
@@ -10,12 +10,17 @@ def clean_dataset(data):
     
     data.drop(['Context'],axis = 1, inplace = True)
     
-    print('~~Dataset Cleaned')
+    print('~~Dataset Cleaned!!')
     
     data= data.replace(',','',regex=True)
     
     data.drop_duplicates(inplace=True)
     
     print('~~Duplicated Entries Removed')
+    
+    data['u_id']= data['Crime ID']+data['Month']+data['Reported by']+data['Location']+data['LSOA code']+data['Last outcome category']+ str(data['Longitude']*data['Latitude'])
+    data['UN_ID']=data['u_id'].apply(hash)
+    data.drop(['u_id'],axis = 1, inplace = True)
+    print('~~UniqueID Created!!')
     
     return data
